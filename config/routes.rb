e2aa17c0dd37e3990686f78162ba37dc738a6730
k42un0k0 <k42un0k0.put1put1@gmail.com>
2021-05-30 20:05:21 +0900

    add scaffold

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :responses
+  resources :boards
   resources :timetables
   resources :animes
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
